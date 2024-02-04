ff7534f7635a4e6d5cae894752fe5d1cd75026e7
pranith213 <126413130+pranith213@users.noreply.github.com>
2024-02-04 22:29:23 +0530

    Add files via upload

---
+++ Jagged-Array-Implementation-class20-CipherSchools-main/JaggedArrays.java
@@ -0,0 +1,30 @@
+public class JaggedArrays {
+    static void printJaggedArray(int arr[][]){
+         // Printing jagged array properly
+    //all rows in separate lines
+    for(int i = 0; i < arr.length; i++){ //rows
+        for(int j = 0; j < arr[i].length; j++){ //columns
+            System.out.println(arr[i][j] + " ");
+        }
+        System.out.println();
+    }
+    }
+    public static void main(String[] args) {
+    int arr[][] = new int[3][];
+    arr[0] = new int[3];
+    arr[1] = new int[2];
+    arr[2] = new int[3];
+    printJaggedArray(arr);
+    
+    int arr2[][] = new int[3][];
+    /* arr2[0] = {1,2,3}; //we cannot use initializers list here because it doesn't create an object
+    arr2[1] = {2,3};
+    arr2[2] = {4,5,6}; */ 
+
+    arr2[0] = new int[] {1,2,3};
+    arr2[1] = new int[] {2,3};
+    arr2[2] = new int[] {4,5,6};
+    System.out.println("*******************");
+    printJaggedArray(arr2);
+    }
+}
